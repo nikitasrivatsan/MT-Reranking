85876ee72164425d6c667d00a9d86f3232d5334b
Nathan Smith <nathansforyou@gmail.com>
2016-03-30 17:16:01 -0400

    additional feature for sentence length

---
+++ sent_diff_feature.py
@@ -0,0 +1,13 @@
+import optparse
+import sys
+
+def main():
+    input_file = open(sys.argv[1])
+    output_file = open(sys.argv[2], 'w')
+
+    for line in input_file:
+        line_length = len(line.split(' ||| ')[1].split())
+        outfile.write(line.strip() + " sentence_length=" + str(line_length) + "\n")
+
+if __name__ == "__main__":
+    main()
